beda4e14ca687dd2413668583770c124a0316893
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-04-01 17:46:17 +0530

    Time: 29 ms (52.81%), Space: 61.8 MB (48.94%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.java
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int subarraysDivByK(int[] A, int K) {
+    public int subarraysDivByK(int[] nums, int K) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
         int count = 0, sum = 0;
-        for(int a : A) {
+        for(int a : nums) {
             sum = (sum + a) % K;
-            if(sum < 0) sum += K;  // Because -1 % 5 = -1, but we need the positive mod 4
+            if(sum < 0) sum += K;  
             count += map.getOrDefault(sum, 0);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
