87d842fd39658c63ae3b89e5b25d6829492e3ab4
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-04-11 18:28:02 +0530

    Time: 2 ms (33.48%), Space: 42.9 MB (32.99%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
@@ -1,28 +1,27 @@
 class Solution {
-    Map<Character, Integer> map;
     public boolean isAlienSorted(String[] words, String order) {
-        map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < order.length(); i++) {
             map.put(order.charAt(i), i);
         }
-        for (int i = 0; i < words.length - 1; i++) {
-            if (!compare(words[i], words[i + 1])) return false;
-        }
-        return true;
-    }
-    
-    private boolean compare(String s1, String s2) {
-        int l1 = s1.length(), l2 = s2.length();
-        for (int i = 0, j = 0; i < l1 && j < l2; i++, j++) {
-            if (s1.charAt(i) != s2.charAt(j)) {
-                if (map.get(s1.charAt(i)) > map.get(s2.charAt(j))) {
+        for (int i = 0; i < words.length -1; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                
+                if (j >= words[i + 1].length()) return false;
+                
+                char cur = words[i].charAt(j);
+                char next = words[i + 1].charAt(j);
+                
+                if (map.get(cur) > map.get(next)) {
                     return false;
-                } else {
-                    return true;
+                }
+                else if(map.get(cur) == map.get(next)){
+                    continue;
+                }else {
+                    break;
                 }
             }
         }
-        if (l1 > l2) return false;
         return true;
     }
 }
