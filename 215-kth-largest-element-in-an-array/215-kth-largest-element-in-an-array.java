de044a3d37f8dffb3ff10e10e2ca384cae47eb34
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-05-01 16:29:35 +0530

    Time: 4 ms (77.21%), Space: 44 MB (68.94%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,34 +1,15 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-      return quickSelect(nums, 0, nums.length - 1, k);
-    }
-
-    int quickSelect(int[] nums, int low, int high, int k) {
-      int pivot = low;
-
-      // use quick sort's idea
-      // put nums that are <= pivot to the left
-      // put nums that are  > pivot to the right
-      for (int j = low; j < high; j++) {
-        if (nums[j] <= nums[high]) {
-          swap(nums, pivot++, j);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i=0;i<k;i++){
+            minHeap.add(nums[i]);
         }
-      }
-      swap(nums, pivot, high);
-
-      // count the nums that are > pivot from high
-      int count = high - pivot + 1;
-      // pivot is the one!
-      if (count == k) return nums[pivot];
-      // pivot is too small, so it must be on the right
-      if (count > k) return quickSelect(nums, pivot + 1, high, k);
-      // pivot is too big, so it must be on the left
-      return quickSelect(nums, low, pivot - 1, k - count);
-    }
-    
-    private void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
+        for(int i=k;i<nums.length;i++){
+            if(nums[i] > minHeap.peek()){
+                minHeap.poll();
+                minHeap.add(nums[i]);
+            }
+        }
+        return minHeap.peek();
     }
 }
