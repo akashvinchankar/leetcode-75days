d4c6845d5300d50bf9082789802b812da30f4ba5
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-05-24 19:28:24 +0530

    Time: 4 ms (14.64%), Space: 56.7 MB (46.34%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.java
+++ 485-max-consecutive-ones/485-max-consecutive-ones.java
@@ -7,11 +7,11 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 1) {
                 count++;
-            } else {
                 max = Math.max(max, count);
+            } else {
                 count = 0;
             }
         }
-        return Math.max(max, count);
+        return max;
     }
 }
