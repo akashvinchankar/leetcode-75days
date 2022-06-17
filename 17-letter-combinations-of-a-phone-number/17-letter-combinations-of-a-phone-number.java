20c89db1dd96ffdc65d193e89f2aef032783739b
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-06-17 17:02:55 +0530

    Time: 1 ms (93.51%), Space: 43.1 MB (29.33%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,24 +1,45 @@
-public class Solution {
-        public static List<String> letterCombinations(String digits) {
-            String digitletter[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-            List<String> result = new ArrayList<String>();
+class Solution {
+    static String[] letters = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     
-            if (digits.length()==0) return result;
+    List<String> results;
+    public List<String> letterCombinations(String digits) {
+        results = new ArrayList();
+        
+        //call this Backtracking method,
+        //add all letter combination in list
+        backtracking(digits, 0, new StringBuilder());
+        
+        return results;
+    }
+    
+    private void backtracking(String digits, int index, StringBuilder sb){
+        
+        //base case
+        //all dgits are covered
+        if(index == digits.length()){
             
-            result.add("");
-            for (int i=0; i<digits.length(); i++) 
-                result = combine(digitletter[digits.charAt(i)-'0'],result);
-            
-            return result;
+            //add this combination in the list
+            if(sb.length() > 0)
+                results.add(sb.toString());
+            return;
         }
         
-        public static List<String> combine(String digit, List<String> l) {
-            List<String> result = new ArrayList<String>();
+        //current digit
+        int digit = digits.charAt(index) - '0';
+        
+        for(char letter : letters[digit].toCharArray()){
+            //add this leeter
+            sb.append(letter);
             
-            for (int i=0; i<digit.length(); i++) 
-                for (String x : l) 
-                    result.add(x+digit.charAt(i));
-    
-            return result;
+            
+            backtracking(digits, index + 1, sb);
+            
+            //remove last added letter
+            //to explore the different permuation of letters
+            sb.deleteCharAt(sb.length() - 1);
         }
+        
     }
+    
+    
+}
