9a98897f08e1d29c1b8a9a5c219f8d02561366f6
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-06-22 11:13:10 +0530

    Time: 1 ms (96.43%), Space: 44.1 MB (82.42%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,22 +1,56 @@
 class Solution {
+
     public void setZeroes(int[][] matrix) {
-        int col0 = 1, rows = matrix.length, cols = matrix[0].length;
-        for (int i = 0; i < rows; i++) {
-            if (matrix[i][0] == 0)
-                col0 = 0;
-            for (int j = 1; j < cols; j++) {
-                if (matrix[i][j] == 0)
-                    matrix[i][0] = matrix[0][j] = 0;
+        Boolean isRow = false;
+        Boolean isCol = false;
+
+        // Check for 1st row element is 0
+        for (int j = 0; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                isRow = true;
+                break;
             }
         }
 
-        for (int i = rows - 1; i >= 0; i--) {
-            for (int j = cols - 1; j >= 1; j--)
-                if (matrix[0][j] == 0 || matrix[i][0] == 0)
+        // Check for 2nd row element is 0
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                isCol = true;
+                break;
+            }
+        }
+
+        // Setting the first row and column to 0 as an reference
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        // Setting rest of the matrix elements to 0 from the reference row and columns
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
                     matrix[i][j] = 0;
+                }
+            }
+        }
 
-            if (col0 == 0) matrix[i][0] = 0;
+        // Setting first row to zero if required
+        if (isRow) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
 
+        // Setting first column to zero if required
+        if (isCol) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
         }
     }
 }
