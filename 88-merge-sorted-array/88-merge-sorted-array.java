77586487bf4ded46d0faf5e71c8a0e9fb23869dd
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-06-23 12:38:32 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (81.12%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,24 +1,24 @@
 class Solution {
-  public void merge(int[] nums1, int m, int[] nums2, int n) {
-    int end = m + n - 1;
-    // backward from nums1
-    int index1 = m - 1;
-    int index2 = n - 1;
-    while (index1 >= 0 && index2 >= 0) {
-      if (nums1[index1] > nums2[index2]) {
-        nums1[end] = nums1[index1];
-        index1--;
-      } else {
-        nums1[end] = nums2[index2];
-        index2--;
-      }
-      end--;
-    }
 
-    // left element
-    while (index2 >= 0) {
-      nums1[index2] = nums2[index2];
-      index2--;
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int total = m + n - 1;
+        int idx1 = m - 1;
+        int idx2 = n - 1;
+
+        while (idx1 >= 0 && idx2 >= 0) {
+            if (nums1[idx1] > nums2[idx2]) {
+                nums1[total] = nums1[idx1];
+                idx1--;
+            } else {
+                nums1[total] = nums2[idx2];
+                idx2--;
+            }
+            total--;
+        }
+
+        while (idx2 >= 0) {
+            nums1[idx2] = nums2[idx2];
+            idx2--;
+        }
     }
-  }
 }
