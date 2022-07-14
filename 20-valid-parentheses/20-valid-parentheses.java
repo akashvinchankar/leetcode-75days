4891ac80abbdc0c09c034c356d6ca4e52851a4b2
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-07-14 16:49:29 +0530

    Time: 4 ms (30.61%), Space: 41.8 MB (72.07%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -5,23 +5,21 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (ch == '(' || ch == '{' || ch == '[') {
+            if (ch == '(' || ch == '[' || ch == '{') {
                 stk.push(ch);
             } else if (stk.size() != 0) {
-                if (ch == ')') {
-                    if (stk.pop() != '(') return false;
-                } else if (ch == '}') {
-                    if (stk.pop() != '{') return false;
-                } else if (ch == ']') {
-                    if (stk.pop() != '[') return false;
+                if (ch == ')' && stk.pop() != '(') {
+                    return false;
+                } else if (ch == ']' && stk.pop() != '[') {
+                    return false;
+                } else if (ch == '}' && stk.pop() != '{') {
+                    return false;
                 }
-                // if stk size is zero
             } else {
                 return false;
             }
         }
-        // after all operations if still the is any element in the stk then the string is disbalanced
-        if (stk.size() > 0) {
+        if (stk.size() != 0) {
             return false;
         }
         return true;
