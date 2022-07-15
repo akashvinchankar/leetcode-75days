ccf9ca6b3fb48eece8d92f0fbf7e3f9d666d2403
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-07-15 16:02:55 +0530

    Time: 4 ms (85.70%), Space: 43.8 MB (54.35%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,17 +1,18 @@
 class Solution {
 
     public boolean isAnagram(String s, String t) {
+        
         if (s.length() != t.length()) return false;
 
-        int[] count = new int[26];
+        int[] arr = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
         }
 
-        for (int i : count) {
-            if (i != 0) {
+        for (int ele : arr) {
+            if (ele != 0) {
                 return false;
             }
         }
