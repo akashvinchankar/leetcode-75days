55f3024946e5553cdef9c20d5cca76031cab8ca3
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-07-17 12:25:22 +0530

    Time: 2 ms (92.90%), Space: 42.3 MB (60.16%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,23 +1,37 @@
 class Solution {
 
     public int longestPalindrome(String s) {
-        if (s.length() == 0 || s == null) return 0;
+        //         if (s.length() == 0 || s == null) return 0;
 
-        HashSet<Character> hset = new HashSet<>();
+        //         HashSet<Character> hset = new HashSet<>();
 
-        int count = 0;
+        //         int count = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (hset.contains(s.charAt(i))) {
-                hset.remove(s.charAt(i));
-                count++;
-            } else {
-                hset.add(s.charAt(i));
+        //         for (int i = 0; i < s.length(); i++) {
+        //             if (hset.contains(s.charAt(i))) {
+        //                 hset.remove(s.charAt(i));
+        //                 count++;
+        //             } else {
+        //                 hset.add(s.charAt(i));
+        //             }
+        //         }
+        //         if (hset.isEmpty()) {
+        //             return count * 2;
+        //         }
+        //         return count * 2 + 1;
+
+        int[] arr = new int[128];
+        for (char c : s.toCharArray()) {
+            arr[c]++;
+        }
+
+        int res = 0;
+        for (int i : arr) {
+            res = res + (i / 2 * 2);
+            if (res % 2 == 0 && i % 2 == 1) {
+                res++;
             }
         }
-        if (hset.isEmpty()) {
-            return count * 2;
-        }
-        return count * 2 + 1;
+        return res;
     }
 }
